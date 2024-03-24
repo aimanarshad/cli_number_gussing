9d7bbc36ae2c08136d1e7c56fb9b346b73c6c723
Aiman <aimanat2022@gmail.com>
2024-03-24 13:38:14 +0500

    changes in if

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 
-const randomNumber =Math.floor(Math.random()* 10+1);
-console.log(randomNumber);
+const randomNumbers =Math.floor(Math.random()* 10+1);
+console.log(randomNumbers);
 const answer=await inquirer.prompt([{
     name:"UserGusesNumber",
     type:"number",
     message:"just guess a number in range of 1-10",
 }]);
-if(answer.UserGusesNumber===randomNumber){
+if(answer.UserGusesNumber===randomNumbers){
 
     console.log("youe guess right number");
 }
